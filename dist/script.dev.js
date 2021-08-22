d6cca6c8d9094b7bab70e555bb7702ac02314824
LeylaHaciyeva <l.hajizadeh21@gmail.com>
2021-08-22 11:27:34 +0300

    projects

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,12 @@
 "use strict";
 
-$(document).ready(function () {// $(".fa-bars").click((e) => {
-  //     $(".navbar__mobile").toggle().toggleClass("active")
-  //     $(".fa-bars").toggleClass("fa-times")     
-  // })
+$(window).on("load", function () {
+  $(".content").css("display", "block");
+  $(".loading").css("display", "none");
+});
+$(document).ready(function () {
+  $(".fa-bars").click(function (e) {
+    $(".navbar__mobile").toggleClass("active");
+    $(".fa-bars").toggleClass("fa-times");
+  });
 });
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-$(document).on("load", function () {
-    $(".loading").css("display", "flex").css("justify-content", "center").css("align-items", "center")
-    $("body").css("overflow", "hidden")
+$(window).on("load", function () {
+    $(".content").css("display","block")
+    $(".loading").css("display","none")
 });
 
 $(document).ready(function () {
-    $(".loading").css("display", "none")
-    $("body").css("overflow", "visible")
     $(".fa-bars").click((e) => {
         $(".navbar__mobile").toggleClass("active")
         $(".fa-bars").toggleClass("fa-times")
