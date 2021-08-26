5a50d23af14ec6c40fa21aa5728b85d8b76b56d6
LeylaHaciyeva <l.hajizadeh21@gmail.com>
2021-08-26 18:29:31 +0300

    finish

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(window).on("load", function () {
-    $(".content").css("display","block")
-    $(".loading").css("display","none")
+    $(".content").css("display", "block")
+    $(".loading").css("display", "none")
 });
 
 $(document).ready(function () {
@@ -8,4 +8,33 @@ $(document).ready(function () {
         $(".navbar__mobile").toggleClass("active")
         $(".fa-bars").toggleClass("fa-times")
     })
+
+    $("#projects__link, #projects__mobile__link").click(function (e) {
+        e.preventDefault()
+        if($(".navbar__mobile").hasClass("active")){
+            $(".navbar__mobile").removeClass("active")
+            $(".fa-bars").removeClass("fa-times")
+        }
+        $("body, html").animate({
+            scrollTop: $("#projects__section").offset().top-50
+        }, 2000);
+    })
+    $("#about__link, #about__mobile__link").click(function (e) {
+        e.preventDefault()
+        
+        if($(".navbar__mobile").hasClass("active")){
+            $(".navbar__mobile").removeClass("active")
+            $(".fa-bars").removeClass("fa-times")
+        }
+        $("body, html").animate({
+            scrollTop: $("#about__section").offset().top
+        }, 2000);
+    })
+    // $("#about__mobile__link").click(function (e) {
+    //     e.preventDefault()
+    //     $("body, html").animate({
+    //         scrollTop: $("#about__section").offset().top
+    //     }, 2000);
+    // })
+   
 })
